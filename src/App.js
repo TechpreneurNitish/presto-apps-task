70a73f26d4cfb11e88426839bc1ff9508bc67b5d
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-15 02:44:46 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Greet from './Components/Greet';
+import Msg from './Components/Msg';
 import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Nitish" heroName="Developer">
+      {/* <Greet name="Nitish" heroName="Developer">
         <p>This is my paragraph for Nitish Developer</p>
         </Greet>
       <Greet name="Rahul" heroName="Coder">
@@ -15,7 +16,8 @@ function App() {
       <Greet name="Barun" heroName="Programmer" />
       <Welcome name="Nitish" heroName="Developer" />
       <Welcome name="Rahul" heroName="Coder" />
-      <Welcome name="Barun" heroName="Programmer" />
+      <Welcome name="Barun" heroName="Programmer" /> */}
+      <Msg  />
 
     </div>
   );
