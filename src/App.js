166c29810e42dbe26c372a2fe932df2be016c9b0
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-15 02:18:37 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Greet name="Nitish" heroName="Developer">
+        <p>This is my paragraph for Nitish Developer</p>
+        </Greet>
+      <Greet name="Rahul" heroName="Coder">
+        <button>Click Me</button>
+        </Greet>
+      <Greet name="Barun" heroName="Programmer" />
+      <Welcome name="Nitish" heroName="Developer" />
+      <Welcome name="Rahul" heroName="Coder" />
+      <Welcome name="Barun" heroName="Programmer" />
 
     </div>
   );
--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Greet = () => <h1>Hello Meri Jaan</h1>
+const Greet = (props) => {
+  console.log(props);
+return (
+  <div>
+    <h1>Hello {props.name} your role is {props.heroName}</h1>
+    {props.children}
+  </div>
+)
+}
 
 export default Greet;
