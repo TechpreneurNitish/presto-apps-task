c674d47dc18324c1e736685d1fe09a7fb475fc8f
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-16 12:24:26 +0530

    guwgesu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MyComp from './Components/MyComp';
 
 function App() {
   return (
     <div className="App">
-      
+      <MyComp />
     </div>
   );
 }
---
+++ src/Components/MyComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyComp() {
+  return (
+    <div>
+      <h1>Kya Meri Jaan</h1>
+    </div>
+  )
+}
+
+export default MyComp
