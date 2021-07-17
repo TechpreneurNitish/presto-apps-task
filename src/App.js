058bd41b1048d79e3ccf88530152822c2a82ffe7
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-17 13:38:28 +0530

    lol

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import ComponentC from './Components/ComponentC';
+import { UserProvider } from './Components/userContext';
 
 function App() {
   return (
     <div className="App">
-      
+      <UserProvider value='Nitish Singh Chauhan'>
+      <ComponentC />
+      </UserProvider>
     </div>
   );
 }
