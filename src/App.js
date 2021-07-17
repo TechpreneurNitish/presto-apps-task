99bc4a0049bb3137f05f985b65f2d9adfcd9659d
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-17 13:06:24 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import './App.css';
-import ClickCounter from './Components/ClickCounter';
-import ClickCounterTwo from './Components/ClickCounterTwo';
-import FocusInput from './Components/FocusInput';
-import FRPreInput from './Components/FRPreInput';
-import Hero from './Components/Hero';
-import HoverCounter from './Components/HoverCounter';
-import HoverCounterTwo from './Components/HoverCounterTwo';
-import Input from './Components/Input';
-import PreComp from './Components/PreComp';
-import RefsDemo from './Components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
-      <ClickCounterTwo />
-      <HoverCounterTwo />
-      {/* 
-      <ClickCounter />
-      <HoverCounter />
-      <Hero />
-      <FRPreInput />
-      <FocusInput />
-      <Input />
-      <RefsDemo />
-      <PreComp />
-      */}
+      
     </div>
   );
 }
