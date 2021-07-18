98da344a598083d28507f958141b29384fd8ad21
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-18 17:21:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import './App.css';
-import ClassCounter from './Components/ClassCounter';
-import CountDown from './Components/CountDown';
-import CounterThree from './Components/CounterThree';
-import CountOne from './Components/CountOne';
-import CountTwo from './Components/CountTwo';
-import DataFetching from './Components/DataFetching';
-import HookCounter from './Components/HookCounter';
-import HookCounterArr from './Components/HookCounterArr';
-import HookCounterOne from './Components/HookCounterOne';
-import HookCounterTwo from './Components/HookCounterTwo';
-import HookMouse from './Components/HookMouse';
-import HookObj from './Components/HookObj';
-import IntervalCounter from './Components/IntervalCounter';
-import MouseCon from './Components/MouseCon';
 
 function App() {
   return (
     <div className="App">
-      <CountTwo />
-      {/* 
-      <CountOne />
-      <DataFetching />
-      <IntervalCounter />
-      <MouseCon />
-      <HookMouse />
-      <HookCounterOne />
-      <CountDown />
-      <HookCounterArr />
-      <CounterThree />
-      <HookObj />
-      <HookCounterTwo />
-      <HookCounter />
-      <ClassCounter /> 
-      */}
+      Pudina lelo pudina
     </div>
   );
 }
