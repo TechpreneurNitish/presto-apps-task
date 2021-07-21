bdec18b6fb613ed43aa8367622f82c6a14f8f621
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-21 16:15:39 +0530

    hjhh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import FetchingData from './Components/FetchingData';
 
 function App() {
   return (
     <div className="App">
-      Hi Dear
     </div>
   );
 }
