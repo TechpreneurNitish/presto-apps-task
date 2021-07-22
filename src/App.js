3a76c4c5d4706973f9893a6133613fd2dc2c7c3b
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-07-22 20:24:13 +0530

    hhhi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import FetchingData from './Components/FetchingData';
 
 function App() {
   return (
     <div className="App">
-      Kya Ammu
+      Hello React
     </div>
   );
 }
