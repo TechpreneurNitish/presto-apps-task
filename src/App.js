a6072299e98d143fd0f94172dbd08050b5217cbf
Nitish Singh Chauhan <kumar.nitish607@gmail.com>
2021-08-10 23:23:46 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      Hello Sharwan
+      <Greet />
+      <Welcome />
     </div>
   );
 }
