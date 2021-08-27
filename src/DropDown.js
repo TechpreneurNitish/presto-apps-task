5c44d567f4c320d603a835990d80278ed6533ec1
Nitish Singh Chauhan <Kumar.nitish607@gmail.com>
2021-08-27 14:43:46 +0530

    HI

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const DropDown = () => {
+  const [dropdown, setDropdown] = useState('Bihar');
 
-  const [dropdown, setDropdown] = useState("Delhi");
-   
-  
-  
+  const dropdownHandler = (event) => {
+    setDropdown(event.target.value);
+  };
+
   return (
     <div>
-    
+      Select state to know capital city : <select value={dropdown} onChange={dropdownHandler}>
+        <option style={{ backgroundColor: 'blue' }} value="Patna">
+          Bihar
+        </option>
+        <option style={{ backgroundColor: 'orange' }} value="Lucknow">
+          Uttar Pradesh
+        </option>
+        <option style={{ backgroundColor: 'red' }} value="Ranchi">
+          Jharkhand
+        </option>
+      </select>
+      <h4>Capital of state is : {dropdown} </h4>
     </div>
-  )
-}
+  );
+};
 
-export default DropDown
+export default DropDown;
