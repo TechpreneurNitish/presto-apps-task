d023c8c9bf3759554d525ff852fec16f1f710d0f
Nitish Singh Chauhan <Kumar.nitish607@gmail.com>
2021-08-27 14:21:30 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import DropDown from './DropDown';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <DropDown />
     </div>
   );
 }
---
+++ src/DropDown.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const DropDown = () => {
+
+  const [dropdown, setDropdown] = useState("Delhi");
+   
+  
+  
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default DropDown
