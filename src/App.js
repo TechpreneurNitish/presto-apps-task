da988c9e4b448361a6873eecba41be1b1f10a8b4
Nitish Singh Chauhan <Kumar.nitish607@gmail.com>
2021-08-28 23:34:57 +0530

    NewH

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import DropDown from './DropDown';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <DropDown />
     </div>
   );
 }
---
+++ src/HTMLPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HTMLPage = () => {
+  return (
+    <div>
+      <h1>HTML Page</h1>
+    </div>
+  )
+}
+
+export default HTMLPage
